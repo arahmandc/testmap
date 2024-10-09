78d827c6d1075fb5dae5f8e7dbd8ec78f2fd6a75
Atikur Rahman <55056373+arahmandc@users.noreply.github.com>
2024-10-09 14:03:17 +0600

    Update main.js

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ function addGeoms(data) {
   // All the rows will be inserted into a single GeoJSON
   let fc = {
     type: "Feature",
-    properties: [],
+    features: [],
   };
 
   for (let row in data) {
@@ -101,8 +101,8 @@ function addGeoms(data) {
   }
 
   // The geometries are styled slightly differently on mouse hovers
-  let geomStyle = { color: "#2ca25f", weight: 2 };
-  let geomHoverStyle = { color: "green", weight: 3 };
+  let geomStyle = { color: "#2ca25f", fillColor: "#99d8c9", weight: 2 };
+  let geomHoverStyle = { color: "green", fillColor: "#2ca25f", weight: 3 };
 
   L.geoJSON(fc, {
     onEachFeature: function (feature, layer) {
@@ -211,7 +211,7 @@ function addPoints(data) {
 function parseGeom(gj) {
   // FeatureCollection
   if (gj.type == "Feature") {
-    return gj.properties;
+    return gj.features;
   }
 
   // Feature
